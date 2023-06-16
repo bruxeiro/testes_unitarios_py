7f11696192dd3963fd3fbcb44ed43b28b2ff0a5f
Bruxeiro <gabrielrosabueno@hotmail.com>
2023-06-16 11:19:32 -0300

    Criando reports dos testes gerados

--- codigo/bytebank.py
+++ codigo/bytebank.py
@@ -1,6 +1,5 @@
 from datetime import date
 
-
 class Funcionario:
     def __init__(self, nome, data_nascimento, salario):
         self._nome = nome
@@ -16,26 +15,30 @@ class Funcionario:
         return self._salario
 
     def idade(self):
-        data_nascimento_formatada = self._data_nascimento.split('/')
-        ano_nascimento = data_nascimento_formatada[-1]
-
+        data_nasciemnto_quebrada = self._data_nascimento.split('/')
+        ano_nascimento = data_nasciemnto_quebrada[-1]
         ano_atual = date.today().year
         return ano_atual - int(ano_nascimento)
 
     def sobrenome(self):
-        nome_completo = self._nome.strip()
+        nome_completo = self.nome.strip()
         nome_quebrado = nome_completo.split(' ')
         return nome_quebrado[-1]
 
-    def decrescimo_salario(self):
+    def _eh_socio(self):
         sobrenomes = ['Bragança', 'Windsor', 'Bourbon', 'Yamato', 'Al Saud', 'Khan', 'Tudor', 'Ptolomeu']
-        if self._salario >= 100000:
+        return (self._salario >= 100000) and (self.sobrenome() in sobrenomes)
+
+    def decrescimo_salario(self):
+        if self._eh_socio():
             decrescimo = self._salario * 0.1
             self._salario = self._salario - decrescimo
+
+
     def calcular_bonus(self):
         valor = self._salario * 0.1
         if valor > 1000:
-            valor = 0
+            raise Exception('O salário é muito alto para receber um bônus')
         return valor
 
     def __str__(self):
--- test/test_bytebank.py
+++ test/test_bytebank.py
@@ -1,45 +1,57 @@
 from codigo.bytebank import Funcionario
+import pytest
+from pytest import mark
+
 
 class TestClass:
-
-
-#Boa pratica: Fazer o teste o mais verboso possível
-    def test_quando_idade_recebe_13_03_2000_deve_retornar_22(self):
-        # Given - Contexto
-        entrada = '13/03/2000'
+    def test_quando_idade_recebe_13_03_2000_deve_retornar_23(self):
+        entrada = '13/03/2000'  # Given-Contexto
         esperado = 23
+
         funcionario_teste = Funcionario('Teste', entrada, 1111)
+        resultado = funcionario_teste.idade()  # When-ação
 
-        # When - Ação
-        resultado = funcionario_teste.idade()
+        assert resultado == esperado  # Then-desfecho
 
-        # Then - Desfecho
-        assert resultado ==esperado
-
-    def test_quando_sobrenome_recebe_Lucas_Carvalho_deve_retornar_apenas_Carvalho(self):
-
-        #ginven
-        entrada = 'Lucas Carvalho'
+    def test_quando_sobrenome_recebe_luucas_carvalho_deve_retornar_carvalho(self):
+        entrada = ' Lucas Carvalho '  # Given
         esperado = 'Carvalho'
 
-        #When
-        lucas = Funcionario(entrada, 1000, 11/11/2000)
-        resultado = lucas.sobrenome()
+        lucas = Funcionario(entrada, '11/11/2000', 1111)
+        resultado = lucas.sobrenome()  # When
 
-        #Then
         assert resultado == esperado
 
+    @mark.calcular_decrescimo
     def test_quando_decrescimo_salario_recebe_100000_deve_retornar_90000(self):
-
-        #ginven
-        entrada = 100000
+        entrada_salario = 100000  # given
         entrada_nome = 'Paulo Bragança'
         esperado = 90000
 
-        #When
-        funcionario_teste = Funcionario(entrada_nome, '11/11/2000', entrada)
-        funcionario_teste.decrescimo_salario()
-        resultado = funcionario_teste.salario
+        funconario_teste = Funcionario(entrada_nome, '11/11/2000', entrada_salario)
+        funconario_teste.decrescimo_salario()  # when
+        resultado = funconario_teste.salario
 
-        #Then
-        assert resultado == esperado
+        assert resultado == esperado  # then
+
+    @mark.calcular_bonus
+    def test_quando_calcular_bonus_recebe_1000_deve_retornar_100(self):
+        entrada = 1000  # given
+        esperado = 100
+
+        funconario_teste = Funcionario('teste', '11/11/2000', entrada)
+        resultado = funconario_teste.calcular_bonus()  # when
+
+        assert resultado == esperado  # then
+
+    @mark.calcular_bonus
+    def test_quando_calcular_bonus_recebe_100000000_deve_retornar_exception(self):
+        with pytest.raises(Exception):  # Aqui o pytest testa se esta função vai retornar uma Exception
+            entrada = 100000000  # given
+
+            funconario_teste = Funcionario('teste', '11/11/2000', entrada)
+            resultado = funconario_teste.calcular_bonus()  # when
+
+            assert resultado  # then
+
+
